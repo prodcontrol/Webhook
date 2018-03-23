6ed7e0f83dfb52a6f020b021c7715097e3a5e9ef
prodcontrol <37222945+prodcontrol@users.noreply.github.com>
2018-03-23 09:42:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ restService.post("/echo", function(req, res) {
   return res.json({
     speech: speech,
     displayText: speech,
-    source: "webhook"
+    source: "webhook-echo-sample"
   });
 });
 
@@ -116,7 +116,7 @@ restService.post("/audio", function(req, res) {
   return res.json({
     speech: speech,
     displayText: speech,
-    source: "webhook"
+    source: "webhook-echo-sample"
   });
 });
 
@@ -126,7 +126,7 @@ restService.post("/video", function(req, res) {
       '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
     displayText:
       '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-    source: "webhook"
+    source: "webhook-echo-sample"
   });
 });
 
@@ -188,7 +188,7 @@ restService.post("/slack-test", function(req, res) {
   return res.json({
     speech: "speech",
     displayText: "speech",
-    source: "webhook",
+    source: "webhook-echo-sample",
     data: {
       slack: slack_message
     }
